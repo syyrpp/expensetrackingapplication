5808be2e72daf680dbbd7745a7e548c0ed33fe94
syyrpp <calebjenkins17@yahoo.com>
2020-05-21 16:01:49 -0500

    build again

---
+++ client/build/precache-manifest.bdba78e45933e5717fdac336d255282f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad902cb725ed028c84fb75d8aa90428",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24592f6d2e8324540adc",
+    "url": "/static/css/main.729695e9.chunk.css"
+  },
+  {
+    "revision": "be9030e5dc71f6078a85",
+    "url": "/static/js/2.26309635.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26309635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24592f6d2e8324540adc",
+    "url": "/static/js/main.f5c962f6.chunk.js"
+  },
+  {
+    "revision": "88db91e3dbc4df75f127",
+    "url": "/static/js/runtime-main.678649ae.js"
+  }
+]);
